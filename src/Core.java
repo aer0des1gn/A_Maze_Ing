60c971e0d0401b7bdb0c35c36f92f12afb43fc7a
aer0des1gn <kuebler.aaron@gmx.de>
2018-07-06 17:14:43 +0200

    did some basics

--- src/Core.java
+++ src/Core.java
@@ -1,20 +1,72 @@
 import processing.core.PApplet;
+import processing.core.PConstants;
+
+import java.util.ArrayList;
 
 public class Core extends PApplet {
+
+    public static boolean DEBUG = true;
+
+    private ArrayList<Tile> tiles;
+    int tilesX = 40;
+    int tilesY = 30;
+
     public static void main(String[] args) {
         PApplet.main("Core", args);
     }
 
     public void settings() {
-        size(600, 600);
+        size(tilesX * Tile.WIDTH, tilesY * Tile.WIDTH);
     }
 
     public void setup() {
         frameRate(30);
+        tiles = new ArrayList<>();
+        for (int x = 0; x < tilesX; x++) {
+            for (int y = 0; y < tilesY; y++) {
+                tiles.add(new Tile(this, x, y, false));
+            }
+        }
     }
 
     public void draw() {
+        background(255);
         surface.setTitle("A-Maze-Ing, FPS: " + round(frameRate));
-        rect(mouseX,mouseY,40,40);
+        if (!DEBUG)
+            noStroke();
+        else stroke(0);
+        for (Tile t : tiles) {
+            t.draw();
+        }
+    }
+
+    public Tile getTile(int x, int y) {
+        if (x > 0 || y > 0 || x < tilesX - 1 || y < tilesY - 1)
+            return tiles.get(y + tilesY * x);
+        else return null;
+    }
+
+    public void keyPressed() {
+        if (key == 'g') {
+            generateMaze();
+        }
+        if (key == 'd') {
+            DEBUG = !DEBUG;
+        }
+    }
+
+    public void mouseClicked() {
+        Tile clickedOn = getTile(floor(mouseX / Tile.WIDTH), floor(mouseY / Tile.WIDTH));
+        System.out.println(clickedOn.toString());
+        if (mouseButton == RIGHT) {
+            for (Tile t : clickedOn.getNeighbours()) {
+                t.setColor(color(255,0,0));
+                //System.out.println(t.toString());
+            }
+        }
+    }
+
+    public void generateMaze() {
+
     }
 }
---
+++ src/Tile.java
@@ -0,0 +1,57 @@
+import processing.core.PConstants;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Tile {
+
+    Core core;
+
+    public static final int WIDTH = 30;
+
+    private int x;
+    private int y;
+
+    public int color = 255;
+
+    boolean isWall;
+
+    public Tile(Core core, int x, int y, boolean isWall) {
+        this.core = core;
+        this.x = x;
+        this.y = y;
+        this.isWall = isWall;
+    }
+
+    public void draw() {
+        if (isWall) core.fill(0);
+        //farbe feld
+        else core.fill(color);
+        core.rect(x * WIDTH, y * WIDTH, WIDTH, WIDTH);
+        //farbe schrift
+        core.fill(0);
+        if (Core.DEBUG) {
+            core.textAlign(PConstants.LEFT, PConstants.TOP);
+            core.text(toString(), x * WIDTH, y * WIDTH);
+        }
+    }
+
+    public String toString() {
+        return x + "|" + y;
+    }
+
+    public ArrayList<Tile> getNeighbours() {
+        ArrayList<Tile> tiles = new ArrayList<>();
+        tiles.add(core.getTile(x - 1, y));
+        tiles.add(core.getTile(x + 1, y));
+        tiles.add(core.getTile(x, y + 1));
+        tiles.add(core.getTile(x, y - 1));
+        tiles.removeAll(Collections.singleton(null));
+        return tiles;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+}
